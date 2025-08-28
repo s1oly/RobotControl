a4dd16d7a7282d63c7f65958f3a245e711026038
s1oly <shubaniyer@gmail.com>
2025-08-28 17:24:13 -0500

    got subscriber to work, pushing before debugging with correction tuning

--- gelbelt_control.py
+++ gelbelt_control.py
@@ -55,20 +55,18 @@ def angle_callback(msg):
     global angle 
     angle = np.array(msg.data)
 
-def pid_controller(Kp, Ki, Kd, setpoint, measurement, final_time):
-    global integral, previous_error, intial_time
+def pid_controller(Kp, setpoint, measurement):
     error = setpoint - measurement
     p = Kp*error
-    integral = integral + Ki*error*(final_time - intial_time + 0.1)
-    d = Kd*(error - previous_error)/(final_time - intial_time + 0.1)
-    mv = p + integral + d
-    previous_error = error
-    intial_time = final_time
+    mv = p 
     return mv
 
 
 
 if __name__ == '__main__':
+
+    rospy.init_node('gelbelt_control')
+
     rtde_c = rtde_control.RTDEControlInterface("192.168.1.12")
     rtde_r = rtde_receive.RTDEReceiveInterface("192.168.1.12")
     # print(ur_rtde)
@@ -78,10 +76,13 @@ if __name__ == '__main__':
     angle = np.array([0,0,0])
     force = np.array([0,0,0])
 
+
     rospy.Subscriber('/target_angle', std_msgs.msg.Float32MultiArray, angle_callback, queue_size=1)
     rospy.Subscriber('/wrist_sensor/wrench', geometry_msgs.msg.WrenchStamped, force_callback, queue_size=1)
 
-    init_force = force[2]
+    r = rospy.Rate(100)
+
+    init_force = force[2].copy()
   
     while not rospy.is_shutdown():
 
@@ -104,33 +105,29 @@ if __name__ == '__main__':
         init_gelbelt_pos = np.matmul(T_world_eef_init, T_eef_init_to_gelbelt)[:3, 3]
 
         T_world_gelbelt_old = world_gelbelt_init(init_gelbelt_rotMatrix, init_gelbelt_pos)
-        
-        print(force[2])
-        print(init_force)
-        print(angle)
 
         # #Moving the robot down the correct increment --> Best contact is 0.1045 --> also dependent on angle and stuff
 
         if(force[2] - init_force > -45):
-            print("moving down")
-            pos_difference = np.array([0,0,-0.0005])
+            # print("moving down")
+            pos_difference = np.array([0,0,-0.0005]) # 0.0005
             target_pos = init_pos + pos_difference
             target = np.concatenate((target_pos, init_rot))
             rtde_c.servoL(target,0.01, 0.01, 0.1,0.05, 100)
-            print(force[2] - init_force)
         elif(force[2] - init_force < -50):
-            print("moving up")
+            # print("moving up")
             pos_difference = np.array([0,0,0.0005])
             target_pos = init_pos + pos_difference
             target = np.concatenate((target_pos, init_rot))
             rtde_c.servoL(target, 0.01, 0.01, 0.1, 0.05, 100)
         else:
-            print("correcting")
+            # print("correcting")
             # then set pose with correction
-            x_rotation = pid_controller(Kp=0.1, Ki=0, Kd=0, setpoint=0, measurement=angle[2], final_time=0)
-            y_rotation = pid_controller(Kp=0.1, Ki = 0, Kd= 0, setpoint= 0, measurement=angle[1], final_time=0)
-            angle = [0,y_rotation, x_rotation]
-            r  = R.from_euler('ZYX', angle, degrees = True)
+            x_rotation = pid_controller(Kp=0.4, setpoint=0, measurement=angle[2])
+            y_rotation = pid_controller(Kp=0.4, setpoint= 0, measurement=angle[1])
+            correct = [0,y_rotation, x_rotation]
+            print(correct)
+            r  = R.from_euler('ZYX', correct, degrees = True)
             gelbelt_correction_rotMatrix = r.as_dcm()
             target_gelbelt_pos_difference = np.array([0,0,0])
 
@@ -146,5 +143,5 @@ if __name__ == '__main__':
             rtde_c.servoL(target, 0.01, 0.01, 0.1, 0.05, 100)
         
   
-        # rospy.sleep(0.1)
+        rospy.sleep(0.001)
         # rospy.signal_shutdown("A")
